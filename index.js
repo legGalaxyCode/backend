d3d1242094cc080819c2536192cc454db4268e69
Alexey Zhavoronkov <alexey.zhavoronkov2001@gmail.com>
2022-03-22 14:38:09 +0300

    restore

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
-const app = require('express')()
+const express = require('express'),
+  app = express(),
+  http = require('http').createServer(app),
+  io = require('socket.io')(http)
 
 const host = '127.0.0.1'
-const port = 3000
+const port = 7000
 
-app.get('/home', (req, res) => {
-  res.status(200).type('text/plain')
-  res.send('Home page')
+let clients = []
+
+io.on('connection', (socket) => {
+  console.log(`Client with id ${socket.id} connected`)
+  clients.push(socket.id)
+
+  socket.emit('message', "I'm server")
+
+  socket.on('message', (message) =>
+    console.log('Message: ', message)
+  )
+
+  socket.on('disconnect', () => {
+    clients.splice(clients.indexOf(socket.id), 1)
+    console.log(`Client with id ${socket.id} disconnected`)
+  })
 })
 
-app.get('/about', (req, res) => {
-  res.status(200).type('text/plain')
-  res.send('About page')
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => res.render('index'))
+app.get('/clients-count', (req, res) => {
+  res.json({
+    count: io.clients().server.engine.clientsCount,
+  })
 })
 
-app.post('/api/admin', (req, res) => {
-  res.status(200).type('text/plain')
-  res.send('Create admin request')
+//отправка сообщения конкретному клиенту по его id
+app.post('/client/:id', (req, res) => {
+  if (clients.indexOf(req.params.id) !== -1) {
+    io.sockets.connected[req.params.id].emit(
+      'private message',
+      `Message to client with id ${req.params.id}`
+    )
+    return res
+      .status(200)
+      .json({
+        message: `Message was sent to client with id ${req.params.id}`,
+      })
+  } else
+    return res
+      .status(404)
+      .json({ message: 'Client not found' })
 })
 
-app.post('/api/user', (req, res) => {
-  res.status(200).type('text/plain')
-  res.send('Create user request')
-})
-
-app.use((req, res, next) => {
-  res.status(404).type('text/plain')
-  res.send('Not found')
-})
-
-app.listen(port, host, function () {
+http.listen(port, host, () =>
   console.log(`Server listens http://${host}:${port}`)
-})
+)
+
